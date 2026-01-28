b9a2807c351eaa9aa046cf85bdce8d5dbb5181a1
AyalaShaisultan <baurjanetekbaev@gmail.com>
2026-01-28 23:09:26 +0500

    ahkjfn

---
+++ pythonbasics/4.py
@@ -0,0 +1,13 @@
+#setting the data type
+#In Python, the data type is set when you assign a value to a variable:
+x = "Hello, World"
+x = 20
+x = 20.5
+x=["apple", "banana", "cherry"] #and etc
+
+#Setting the specific Data type
+#if we want to specify the data type, you can use the following constructor functions:
+x = str("Hello, World!")
+x = int(20)
+x= float(20.5)
+x = set(("apple","banana","cherry"))
